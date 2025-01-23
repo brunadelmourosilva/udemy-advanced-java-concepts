70c4dd1f5a01a0483810f80e3b6cd124c88478d9
Bruna Delmouro <brunadelmouro@gmail.com>
2025-01-22 21:21:03 -0300

    Lambda expressions - exercises

--- lambda_expressions/src/exercises/BasicLambdas.java
+++ lambda_expressions/src/exercises/BasicLambdas.java
@@ -1,6 +1,7 @@
 package exercises;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -16,14 +17,24 @@ interface Evaluate<T> {
   Boolean isNegative(T t);
 }
 
+interface Functionable<T, R> {
+  R applyThis(T t);
+}
+
 public class BasicLambdas {
 
   public static void main(String[] args) {
 
     BasicLambdas basicLambdas = new BasicLambdas();
+    System.out.println("--------------------");
     basicLambdas.consumer();
+    System.out.println("--------------------");
     basicLambdas.supplier();
+    System.out.println("--------------------");
     basicLambdas.predicate();
+    System.out.println("--------------------");
+    basicLambdas.function();
+    System.out.println("--------------------");
   }
 
   public void consumer() {
@@ -57,6 +68,30 @@ public class BasicLambdas {
     System.out.println(predicateLambda.test(-1));
     System.out.println(predicateLambda.test(1));
 
-    // todo 3c
+    Predicate<Integer> predicateLambdaNumbers = c -> c % 2 == 0;
+    System.out.println(check(4, predicateLambdaNumbers)); // even
+    System.out.println(check(7, predicateLambdaNumbers)); // odd
+
+    Predicate<String> predicateLambdaString = c -> c.startsWith("Mr.");
+    System.out.println(check("Mr. Joe Bloggs", predicateLambdaString));
+    System.out.println(check("“Ms. Ann Bloggs", predicateLambdaString));
+
+    Predicate<Integer> predicateLambdaAge = c -> c >= 18;
+    System.out.println("Is Mike an adult (age 33, 1.8 meters)? " + check(33, predicateLambdaAge));
+    System.out.println("Is Ann an adult (age 13, 1.4 meters)? " + check(13, predicateLambdaAge));
+  }
+
+  public void function() {
+    Functionable<Integer, String> functionalInterfaceLambda =
+        f -> "Number is: ".concat(String.valueOf(f));
+    System.out.println(functionalInterfaceLambda.applyThis(25));
+
+    Function<Integer, String> functionLambda =
+        f -> "Number is (Function interface): ".concat(String.valueOf(f));
+    System.out.println(functionLambda.apply(25));
+  }
+
+  private <T> boolean check(T t, Predicate<T> lambda) {
+    return lambda.test(t);
   }
 }
